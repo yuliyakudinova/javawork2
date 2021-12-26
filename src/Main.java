0cdae3afa3aee7f40837fa74ac58251530d57c93
yulechka.kudinova@mail.ru <yulechka.kudinova.1991@mail.ru>
2021-12-26 12:21:43 +0300

    firs commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        int ticketPrice = 130;
+        int rublesPerMile = 20;
+
+        int bonusMile = ticketPrice / rublesPerMile;
+
+        System.out.println("Ваши бонусные мили:");
+        System.out.println( bonusMile + "мили" );
+
+    }
+}
